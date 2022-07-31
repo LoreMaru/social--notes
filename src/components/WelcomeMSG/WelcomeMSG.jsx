dfe90bb934fea4f94f7338e987a4cf9916de98fb
LoreMaru <gialma1120@hotmail.it>
2022-08-01 00:01:00 +0200

    Fix: miglioramentigrafici

--- src/components/WelcomeMSG/WelcomeMSG.jsx
+++ src/components/WelcomeMSG/WelcomeMSG.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 
 const WelcomeMSG = () => {
   return (
-    <div>
+    <div className="WelcomeMSG">
       <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/d4156f91706591.5e3906ec9acf9.gif" />
       <h1>Benvenuto su Social Notes!</h1>
       <h2>Iscriviti o accedi per postare</h2>
