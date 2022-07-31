fd4e7b72bc80e2b709498ad700f704e62d79d929
LoreMaru <gialma1120@hotmail.it>
2022-08-01 00:37:49 +0200

    Fix: miglioramenti grafici

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,26 +9,30 @@ const Navbar = ({ setMainModalContent }) => {
   return (
     <div className="Navbar">
       <Logo />
-      <SectionIcon
-        IconIMGlink={"https://twemoji.maxcdn.com/v/latest/svg/1f465.svg"}
-        altText={"Utenti"}
-        onHandleClick={() => setMainModalContent(<FriendCardList />)}
-      />
-      <SectionIcon
-        IconIMGlink={"https://twemoji.maxcdn.com/v/latest/svg/2709.svg"}
-        altText={"Messaggi"}
-        onHandleClick={() => setMainModalContent(<MessageCardList />)}
-      />
-      <SectionIcon
-        IconIMGlink={"https://twemoji.maxcdn.com/v/latest/svg/1f50f.svg"}
-        altText={"Area Personale"}
-        onHandleClick={() => setMainModalContent(<AreaPersonale />)}
-      />
-      <div className="Navbar__actualUser">
-        <p className="Navbar__actualUser--name">
-          {localStorage.getItem("username")}
-        </p>
-      </div>
+      <span className="Navbar__icon">
+        <SectionIcon
+          IconIMGlink={"https://twemoji.maxcdn.com/v/latest/svg/1f465.svg"}
+          altText={"Utenti"}
+          onHandleClick={() => setMainModalContent(<FriendCardList />)}
+        />
+        <SectionIcon
+          IconIMGlink={"https://twemoji.maxcdn.com/v/latest/svg/2709.svg"}
+          altText={"Messaggi"}
+          onHandleClick={() => setMainModalContent(<MessageCardList />)}
+        />
+        <SectionIcon
+          IconIMGlink={"https://twemoji.maxcdn.com/v/latest/svg/1f50f.svg"}
+          altText={"Area Personale"}
+          onHandleClick={() => setMainModalContent(<AreaPersonale />)}
+        />
+      </span>
+      <span>
+        <div className="Navbar__actualUser">
+          <p className="Navbar__actualUser--name">
+            {localStorage.getItem("username")}
+          </p>
+        </div>
+      </span>
     </div>
   );
 };
